31c23245478ec5e205ef3627dc84bb70718351f2
D-e-v-e-s-h <164469811+D-e-v-e-s-h@users.noreply.github.com>
2024-07-28 13:34:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementsByClassName('btn-red-sm').addEventListener('click', function () {
+    window.location.href = 'https://www.netflix.com/login';
+});
+
+document.getElementsByClassName('getstarted').addEventListener('click', function () {
+    window.location.href = 'https://www.netflix.com/signup/password?locale=en-IN';
+});
---
+++ tr2.js
@@ -0,0 +1,17 @@
+// script.js
+document.getElementById('dropdownButton').addEventListener('click', function() {
+    document.getElementById('dropdownContent').classList.toggle('show');
+});
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName('dropdown-content');
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
